334b663f76339839eae3c5ea79c2f0e1196d4146
aldopaganijr <aldojpagani@gmail.com>
2022-07-30 10:10:44 -0400

    mock data set

--- src/components/home-third/Third.js
+++ src/components/home-third/Third.js
@@ -6,7 +6,7 @@ const Third = () => {
   return (
 
     <div className='third-container'>
-      <img className='options' src="https://images.unsplash.com/photo-1582201957424-621320ad670d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzR8fGFydHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60" alt="art"/>
+      <img className='options' src="https://images.unsplash.com/photo-1584285418504-010afe0e2676?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1716&q=80" alt="art"/>
       <img className='options' src="https://images.unsplash.com/photo-1579541671172-43429ce17aca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2065&q=80" alt="art"/>
       <img className='options' src="https://images.unsplash.com/photo-1578926375605-eaf7559b1458?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1663&q=80" alt="art"/>
     </div>
